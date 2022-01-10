074fc8188caee0616bffca1e789653c1943909ab
Anastasiia Vynychenko <a.vynychenko@mobidev.biz>
2022-01-10 21:27:36 +0200

    initial commit

---
+++ main_pipeline.py
@@ -0,0 +1,8 @@
+from settings import *
+
+def run_pipeline():
+   return None
+
+
+if __name__ == '__main__':
+    run_pipeline()
---
+++ settings.py
@@ -0,0 +1,7 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+DATA_PATH = f'{basedir}/data'
+
+RAW_DATA_FILE = 'Reviews.csv'
